f1bfcfe1d0ca332d1aa4777dee56165f9c64393d
PeterXay <peterxay@github.com>
2015-06-17 08:30:53 -0700

    ticket #25, display a comment input text area

--- site.php
+++ site.php
@@ -1,5 +1,18 @@
 <?php
+session_start();
 
-echo "Hello World";
+require('db.php');
+
+$userid = $_SESSION['user'];
+
+echo "<b>Please enter your comment:</b>";
+$temp = $_POST['comments'];
+
+echo "<form method='post' action=''>";
+echo "<textarea name='comments' cols='30' rows='6'>";
+echo "</textarea><br>";
+echo "<input type='submit' value='Submit' />";
+echo "</form>";
 
 ?>
+
